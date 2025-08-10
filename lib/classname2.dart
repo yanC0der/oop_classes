0885eeafad33b111df53047cd6cd598cc2962b46
Crosshair2-bit <diobertpatiluna7@gmail.com>
2025-08-10 15:42:22 +0800

    Classname2bit

---
+++ lib/classname2.dart
@@ -0,0 +1,10 @@
+class Classname2 {
+  String property1;
+  int property2;
+
+  Classname2({required this.property1, required this.property2});
+
+  void displayInfo() {
+    print('This is $property1 with value $property2');
+  }
+}
